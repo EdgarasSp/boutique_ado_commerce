d383cf4ce3abd8f9a0a597530a9392bda50f3ad6
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-07-22 18:32:17 +0000

    custom admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):  # SETUPS WHICH COLUMNS TO SHOW IN ADMIN
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)  # SET TO ORDERING NOTE MUST BE A TUPLE I.E IF ONE HAS TO HAVE A COMMA, TO REVERS AD - I.E '-sku'
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin) # here register models i.e 'Product" and classes i.e 'ProductAdmin'
+admin.site.register(Category, CategoryAdmin)
