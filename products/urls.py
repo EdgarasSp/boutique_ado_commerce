2bcfaa51f384af93fbf48baf5b0d78fd02c1a221
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-07-24 12:11:32 +0000

    add product details page

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from . import views  # to import views from .py needed to display urls
 
 
 urlpatterns = [
-    path('', views.all_products, name='products')  # all_products is from the views, name is product (product being viewed?)
+    path('', views.all_products, name='products'),  # all_products is from the views, name is product (product being viewed?)
+    path('<product_id>', views.product_detail, name='product_detail'),  # <product_id> is pased from db to show in url
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product  # imports our Products database
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)  # first 'products/products.html is where html template located, then context is to send things back to the template?
+
+
+def product_detail(request, product_id):  # product id is what item is requested
+    """ A view to show a product, including sorting and search queries """
+
+    product = get_object_or_404(Product, pk=product_id)  # imports a product from the database asper passed parameter 'product_id'
+
+    context = {
+        'product': product,  #add the product to the context so that it could be added to the template
+    }
+
+    return render(request, 'products/product_detail.html', context)  # first 'products/products.html is where html template located, then context is to send things back to the template?
