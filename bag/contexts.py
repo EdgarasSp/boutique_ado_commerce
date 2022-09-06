1d61fd27db66e9a83f0663da81b2b03a91df90de
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-06 14:18:25 +0000

    add quantity +- buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,15 +12,28 @@ def bag_contents(request):
     bag = request.session.get('bag', {})  ## gets bag from context session if does not exist creates new
 
     ### calculates bag total and creates product dictionary which will be used for creating shopping bag items view
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)  # imported products to get ID's
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({   # library to get product details
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)  # imported products to get ID's
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({       # library to get product details
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)    # settings means from settings.py het variable STANDARD_DELIVERY_PERCENTAGE. total divided by % set
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+# session is used until client and server stops comunicating meaning user closes teh sessions so data is no longer stored.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -13,13 +14,25 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})   # session is used until client and server stops comunicating meaning user closes teh sessions so data is no longer stored.
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+    
