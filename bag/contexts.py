0c484fad4f28a2611b2a09ef9f440641248722c4
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-06 11:22:30 +0000

    add view shopping bag items

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,26 @@
 from decimal import Decimal   ## decimal function used in delivery calc. 
                               #NOTE: use decimal for financial calculations due to rounding
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []  ## variable to save items into
     total = 0        ## set default value to update afer
     product_count = 0  ## set default value to update afer
+    bag = request.session.get('bag', {})  ## gets bag from context session if does not exist creates new
+
+    ### calculates bag total and creates product dictionary which will be used for creating shopping bag items view
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)  # imported products to get ID's
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({   # library to get product details
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)    # settings means from settings.py het variable STANDARD_DELIVERY_PERCENTAGE. total divided by % set
