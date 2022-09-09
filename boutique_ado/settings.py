beb4027dc4ddfed4a92c100093f94c6a8022c865
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-09 10:06:31 +0000

    setup heroku files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0r8$&jl5__^^^i-9ee!mq&v%7$)oq1r215!yz7%of(c+&x9a6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-esp-2022.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,29 +130,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-
-
-#DATABASES = {
-#    'default': dj_database_url.parse() 
-#}
-
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        }
-#    }
 
 
 # Password validation
