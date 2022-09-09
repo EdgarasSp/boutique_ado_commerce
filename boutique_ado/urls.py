647bc493f94733ddfb39e75ba594872da0a5a422
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-09 20:45:41 +0000

    fix

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from .views import handler404
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,7 +24,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 # linking static files css end
 
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
